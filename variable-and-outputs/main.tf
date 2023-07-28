97adc503a55adcd9daa970022a4c9d02362d23ec
Kamlesh Katpara <mekamleshk@gmail.com>
2023-07-29 05:10:11 +0530

    outputs

--- variable-and-outputs/main.tf
+++ variable-and-outputs/main.tf
@@ -8,7 +8,12 @@ terraform {
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
+  description = "The size of the instance."
+  validation {
+    condition     = can(regex("^t2.", var.instance_type))
+    error_message = "The instance must be a t2 type EC2 instance."
+  }
 }
 
 provider "aws" {
