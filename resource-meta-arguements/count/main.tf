069a0ff9af66d52f9138712a2f5604e7f3a1c018
Kamlesh Katpara <mekamleshk@gmail.com>
2023-07-31 04:22:28 +0530

    count

--- resource-meta-arguements/count/main.tf
+++ resource-meta-arguements/count/main.tf
@@ -12,18 +12,13 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "wesohaex-depends-on"
-}
-
-resource "aws_instance" "my_server" {
+resource "aws_instance" "my_app_server" {
+  count         = 2
   ami           = "ami-072ec8f4ea4a6f2cf"
   instance_type = "t2.micro"
-  depends_on = [
-    aws_s3_bucket.bucket
-  ]
+  tags          = { Name = "Server-${count.index}" }
 }
 
 output "public_ip" {
-  value = aws_instance.my_server.public_ip
+  value = aws_instance.my_app_server[*].public_ip
 }
