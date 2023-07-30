d8c5e7e0ac57da92589f571d59033d3f5a1af056
Kamlesh Katpara <mekamleshk@gmail.com>
2023-07-30 18:50:18 +0530

    datasources

--- variable-and-outputs/aws_server/main.tf
+++ variable-and-outputs/aws_server/main.tf
@@ -23,16 +23,29 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-locals {
-  ami           = "ami-072ec8f4ea4a6f2cf"
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "my_app_server" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 }
 
-resource "aws_instance" "my_server" {
-  ami           = local.ami
-  instance_type = local.instance_type
-}
-
 output "public_ip" {
-  value = aws_instance.my_server.public_ip
+  value = aws_instance.my_app_server.public_ip
 }
