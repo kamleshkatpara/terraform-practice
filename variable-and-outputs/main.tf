c2a1caab0b041d49d63388509e8b6c09f723e9f4
Kamlesh Katpara <mekamleshk@gmail.com>
2023-07-30 07:57:29 +0530

    submodules

--- variable-and-outputs/main.tf
+++ variable-and-outputs/main.tf
@@ -1,30 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.9.0"
-    }
-  }
+terraform {}
+
+module "aws_server" {
+  source        = ".//aws_server"
+  instance_type = "t2.micro"
 }
 
-variable "instance_type" {
-  type        = string
-  description = "The size of the instance."
-  validation {
-    condition     = can(regex("^t2.", var.instance_type))
-    error_message = "The instance must be a t2 type EC2 instance."
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "aws_instance" "my_app_server" {
-  ami           = "ami-072ec8f4ea4a6f2cf"
-  instance_type = var.instance_type
-}
-
-output "instance_ip_addr" {
-  value = aws_instance.my_app_server.public_ip
+output "public_ip" {
+  value = module.aws_server.public_ip
 }
